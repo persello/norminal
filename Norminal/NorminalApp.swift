1d548baefd2148ce9abf07953014acfc51af1516
Riccardo Persello <riccardo.persello+i@icloud.com>
2020-10-09 18:59:41 +0200

    Initial Commit

---
+++ Norminal/NorminalApp.swift
@@ -0,0 +1,17 @@
+//
+//  NorminalApp.swift
+//  Norminal
+//
+//  Created by Riccardo Persello on 09/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct NorminalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
