4aa271a39d21e5df44250c39b63dbb4528318444
Riccardo Persello <riccardo.persello+i@icloud.com>
2020-12-19 00:00:15 +0100

    Added feedback tab.
    Signed-off-by: Riccardo Persello <riccardo.persello+i@icloud.com>

--- Norminal/Views/MainView.swift
+++ Norminal/Views/MainView.swift
@@ -7,12 +7,48 @@
 
 import SwiftUI
 
+extension Bundle {
+    public var icon: UIImage? {
+        if let icons = infoDictionary?["CFBundleIcons"] as? [String: Any],
+           let primaryIcon = icons["CFBundlePrimaryIcon"] as? [String: Any],
+           let iconFiles = primaryIcon["CFBundleIconFiles"] as? [String],
+           let lastIcon = iconFiles.last {
+            return UIImage(named: lastIcon)
+        }
+        return nil
+    }
+}
+
 struct MainView: View {
     var body: some View {
         TabView {
             LaunchView()
-                .tabItem { Image(systemName: "flame")
+                .tabItem {
+                    Image(systemName: "flame")
                     Text("Launches")
+                }
+            VStack {
+                HStack {
+                    Image(uiImage: Bundle.main.icon!)
+                        .clipShape(RoundedRectangle(cornerRadius: 12, style: .continuous))
+                    VStack(alignment: .leading) {
+                        Text("Norminal")
+                            .font(.title)
+                        Text("v\(Bundle.main.infoDictionary!["CFBundleShortVersionString"] as! String)")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                    }
+                    
+                }
+                Link(destination: URL(string: "https://forms.gle/1pzmsASLWYjEPnU5A")!, label: {
+                    Text("Join the Alpha channel")
+                })
+                .padding(48)
+            }
+            // .buttonStyle(RoundedButtonStyle())
+            .tabItem {
+                Image(systemName: "exclamationmark.bubble.fill")
+                Text("Feedback")
             }
         }
     }
