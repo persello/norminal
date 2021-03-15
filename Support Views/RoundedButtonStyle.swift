f1e44a1e1138fb088c97f4622f00fdde00c69f20
Riccardo Persello <riccardo.persello+i@icloud.com>
2021-03-15 23:04:44 +0100

    Astronaut picture scaling, RoundedButtonStyle is bold.

--- Support Views/RoundedButtonStyle.swift
+++ Support Views/RoundedButtonStyle.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct RoundedButtonStyle: ButtonStyle {
     func makeBody(configuration: Self.Configuration) -> some View {
         configuration.label
-            .font(.callout)
+            .font(Font.callout.weight(.bold))
             .textCase(.uppercase)
             .foregroundColor(.accentColor)
             .padding(.vertical, 6)
@@ -21,5 +21,16 @@ struct RoundedButtonStyle: ButtonStyle {
                     .foregroundColor(Color(UIColor.systemGray5))
                     .cornerRadius(15)
             )
+            .opacity(configuration.isPressed ? 0.4 : 1.0)
+    }
+}
+
+struct RoundedButton_Previews: PreviewProvider {
+    static var previews: some View {
+        Button(action: {}, label: {
+            Text("Press me")
+        })
+        .buttonStyle(RoundedButtonStyle())
+        .previewLayout(.sizeThatFits)
     }
 }
