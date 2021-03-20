fc2693f2ca93e4541f1efe41d043e13ddc997029
Riccardo Persello <riccardo.persello+i@icloud.com>
2021-03-20 17:31:19 +0100

    iPad navigation complete.

--- Norminal/Views/MainView.swift
+++ Norminal/Views/MainView.swift
@@ -19,40 +19,60 @@ extension Bundle {
     }
 }
 
+enum ViewSelection {
+    case launchList
+    case feedback
+}
+
+struct SidebarButtonStyle: ButtonStyle {
+    private var isActive: Bool = false
+    
+    func getForegroundColor(configuration: Configuration) -> Color? {
+        return configuration.isPressed ? .secondary : nil
+    }
+    
+    func getBackgroundColor(configuration: Configuration) -> Color {
+        if isActive {
+            return Color(UIColor.secondarySystemFill)
+        } else {
+            return .clear
+        }
+    }
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .foregroundColor(getForegroundColor(configuration: configuration))
+            .padding(.vertical, 10)
+            .padding(.leading, 8)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(
+                RoundedRectangle(cornerRadius: 10.0, style: .continuous)
+                    .fill(getBackgroundColor(configuration: configuration))
+            )
+            .opacity(configuration.isPressed ? 0.4 : 1.0)
+            .padding(.horizontal, -8)
+            .padding(.vertical, -10)
+    }
+    
+    func active(_ a: Bool = true) -> SidebarButtonStyle {
+        var style = self
+        style.isActive = a
+        return style
+    }
+}
+
 struct CompactMainView: View {
     var body: some View {
         TabView {
             NavigationView {
                 LaunchListView()
-                    .tabItem {
-                        Image(systemName: "flame")
-                        Text("Launches")
-                    }
+            }
+            .tabItem {
+                Image(systemName: "flame")
+                Text("Launches")
             }
             
-            VStack {
-                HStack {
-                    if let icon = Bundle.main.icon {
-                        Image(uiImage: icon)
-                            .clipShape(
-                                RoundedRectangle(cornerRadius: 12,
-                                                 style: .continuous))
-                    }
-                    
-                    VStack(alignment: .leading) {
-                        Text("Norminal")
-                            .font(.title)
-                        Text("v\(Bundle.main.infoDictionary!["CFBundleShortVersionString"] as! String)")
-                            .font(.subheadline)
-                            .foregroundColor(.gray)
-                    }
-                    
-                }
-                Link(destination: URL(string: "https://forms.gle/1pzmsASLWYjEPnU5A")!, label: {
-                    Text("Join the Alpha channel")
-                })
-                .padding(48)
-            }
+            FeedbackView()
             .tabItem {
                 Image(systemName: "exclamationmark.bubble.fill")
                 Text("Feedback")
@@ -62,10 +82,20 @@ struct CompactMainView: View {
 }
 
 struct SidebarView: View {
+    @Binding var selectedView: ViewSelection
+    
     var body: some View {
         List {
-            Label("Launches", systemImage: "flame")
-            Label("Feedback", systemImage: "exclamationmark.bubble")
+            Button(action: {selectedView = .launchList}) {
+                Label("Launches", systemImage: "flame")
+            }
+            .buttonStyle(SidebarButtonStyle().active(selectedView == .launchList))
+            
+            Button(action: {selectedView = .feedback}) {
+                Label("Feedback", systemImage: "exclamationmark.bubble")
+            }
+            .buttonStyle(SidebarButtonStyle().active(selectedView == .feedback))
+            
         }
         .listStyle(SidebarListStyle())
         .navigationTitle("Norminal")
@@ -73,13 +103,37 @@ struct SidebarView: View {
 }
 
 struct RegularMainView: View {
+    @EnvironmentObject var globalData: SpaceXData
+    @State var selectedView = ViewSelection.launchList
+    
     var body: some View {
-        NavigationView {
-            SidebarView()
-            LaunchListView()
-            LaunchDetailView(launch: FakeData.shared.crewDragon!)
+        
+        switch selectedView {
+            case .launchList:
+                NavigationView {
+                    SidebarView(selectedView: $selectedView)
+                    LaunchListView()
+                    if let nextLaunch = globalData.getNextLaunch() {
+                        LaunchDetailView(launch: nextLaunch)
+                    } else {
+                        VStack {
+                            Text("No details available")
+                                .font(.title)
+                            Text("Select a launch from the sidebar.")
+                                .foregroundColor(.secondary)
+                                .font(.subheadline)
+                        }
+                    }
+                    
+                }
+                .navigationViewStyle(DoubleColumnNavigationViewStyle())
+
+            case .feedback:
+                NavigationView {
+                    SidebarView(selectedView: $selectedView)
+                    FeedbackView()
+                }
         }
-        .navigationViewStyle(DoubleColumnNavigationViewStyle())
     }
 }
 
@@ -97,7 +151,13 @@ struct MainView: View {
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
-        MainView()
-            .environmentObject(SpaceXData.shared)
+        Group {
+            MainView()
+                .environmentObject(SpaceXData.shared)
+                .previewDevice("iPad Air (3rd generation)")
+            
+            SidebarView(selectedView: .constant(.launchList))
+                .previewLayout(.sizeThatFits)
+        }
     }
 }
