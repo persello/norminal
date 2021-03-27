8cfe3e325520622e2a593de5b58b71c6b6c8a95a
Riccardo Persello <riccardo.persello+i@icloud.com>
2021-03-27 20:04:29 +0100

    Conditional import of Firebase based on platform (Mac not supported).

--- Norminal/NorminalApp.swift
+++ Norminal/NorminalApp.swift
@@ -5,7 +5,10 @@
 //  Created by Riccardo Persello on 09/10/2020.
 //
 
+#if !os(macOS) && !targetEnvironment(macCatalyst)
 import Firebase
+#endif
+
 import SwiftUI
 import os
 import WidgetKit
@@ -26,8 +29,10 @@ struct NorminalApp: App {
                         WidgetCenter.shared.reloadAllTimelines()
                     }
                     
+                    #if !os(macOS) && !targetEnvironment(macCatalyst)
                     // Firebase
                     FirebaseApp.configure()
+                    #endif
                 }
                 .environmentObject(globalData)
         }
