f375c4f25b0be4a62510d3db2317f687119375ce
Riccardo Persello <riccardo.persello+i@icloud.com>
2021-05-02 22:04:44 +0200

    History endpoint.

---
+++ Data/SpaceX/History.swift
@@ -0,0 +1,25 @@
+//
+//  History.swift
+//  Norminal
+//
+//  Created by Riccardo Persello on 26/03/21.
+//
+
+import Foundation
+
+class HistoryEntry: ObservableObject, Decodable {
+    struct Links: Decodable {
+        public var article: URL?
+    }
+    
+    public var title: String?
+    public var date: Date?
+    public var details: String?
+    public var links: Links?
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case date = "event_date_utc"
+        case details, links
+    }
+}
--- Data/SpaceX/SpaceXData.swift
+++ Data/SpaceX/SpaceXData.swift
@@ -11,23 +11,25 @@ import os
 import WidgetKit
 
 // MARK: - Utility extension for initializing UUID from string with no dashes
+
 extension UUID {
     init?(stringWithoutDashes input: String) {
         var dashed = input
         while dashed.count < 32 {
             dashed.append("0")
         }
-        
+
         dashed.insert("-", at: input.index(input.startIndex, offsetBy: 20))
         dashed.insert("-", at: input.index(input.startIndex, offsetBy: 16))
         dashed.insert("-", at: input.index(input.startIndex, offsetBy: 12))
         dashed.insert("-", at: input.index(input.startIndex, offsetBy: 8))
-        
+
         self.init(uuidString: dashed.uppercased())
     }
 }
 
 // MARK: - Custom date formatter for extended ISO8601 support.
+
 extension DateFormatter {
     static let iso8601Full: DateFormatter = {
         let formatter = DateFormatter()
@@ -40,12 +42,23 @@ extension DateFormatter {
 }
 
 // MARK: - Custom JSON date format decoder
+
 class CustomDecoder: JSONDecoder {
-    let dateFormatter = DateFormatter.iso8601Full
-    
     override init() {
         super.init()
         dateDecodingStrategy = .formatted(DateFormatter.iso8601Full)
+        dateDecodingStrategy = .custom({ (decoder) -> Date in
+            let container = try decoder.singleValueContainer()
+            let dateStr = try container.decode(String.self)
+            
+            if let date = DateFormatter.iso8601Full.date(from: dateStr) {
+                return date
+            } else if let date = ISO8601DateFormatter().date(from: dateStr) {
+                return date
+            } else {
+                throw DecodingError.dataCorruptedError(in: container, debugDescription: "Cannot decode date string \(dateStr).")
+            }
+        })
     }
 }
 
@@ -53,10 +66,9 @@ class CustomDecoder: JSONDecoder {
 
 /// Contains data obtained from the SpaceX API
 final class SpaceXData: ObservableObject {
-    
     // Logger
     private var logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "SpaceX Data")
-    
+
     // SpaceX data sections
     @Published public var launches = [Launch]()
     @Published public var crew = [Astronaut]()
@@ -67,24 +79,27 @@ final class SpaceXData: ObservableObject {
     @Published public var companyInfo = CompanyInfo()
     @Published public var cores = [Core]()
     @Published public var dragons = [Dragon]()
+    @Published public var fairings = [Fairing]()
+    @Published public var history = [HistoryEntry]()
     @Published var loadingError: Bool = false
-    
+
     // Shared instance
     static var shared = SpaceXData()
-    
+
     // Methods
     func getNextLaunch() -> Launch? {
-        return launches.first(where: {$0.upcoming})
+        return launches.first(where: { $0.upcoming })
     }
-    
+
     // MARK: Generic function for loading data
+
     func loadData<T: Decodable>(url: URL) -> T? {
         var result: T?
         let semaphore = DispatchSemaphore(value: 0)
-        
+
         let task = URLSession.shared.dataTask(with: url) { [self] data, response, error in
-            defer {semaphore.signal()}
-            
+            defer { semaphore.signal() }
+
             if let error = error {
                 logger.error("Error while loading \(T.Type.self) from \(url.absoluteString) due to \"\(error as NSObject)\".")
                 DispatchQueue.main.async {
@@ -92,15 +107,15 @@ final class SpaceXData: ObservableObject {
                 }
                 return
             }
-            
-            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+
+            guard let httpResponse = response as? HTTPURLResponse, (200 ... 299).contains(httpResponse.statusCode) else {
                 logger.error("Error while loading \(T.Type.self) from \(url.absoluteString): server returned non-200 status code.")
                 DispatchQueue.main.async {
                     loadingError = true
                 }
                 return
             }
-            
+
             if let data = data {
                 do {
                     result = try CustomDecoder().decode(T.self, from: data)
@@ -113,27 +128,28 @@ final class SpaceXData: ObservableObject {
                 }
             }
         }
-        
+
         task.resume()
-        
+
         _ = semaphore.wait(timeout: DispatchTime.now() + 10.0)
         return result
     }
-    
+
     func loadAllData() {
-        self.loadingError = false
-        
+        loadingError = false
+
         DispatchQueue.global(qos: .background).async {
             let queue = OperationQueue()
             queue.name = "com.persello.norminal.widget.concurrentTimelineGeneration"
             queue.qualityOfService = .background
             queue.maxConcurrentOperationCount = 8
-            
+
             // MARK: Launches
+
             var _launches: [Launch]?
             queue.addOperation { [self] in
                 _launches = loadData(url: URL(string: "https://api.spacexdata.com/v4/launches")!)
-                
+
                 // Sort launches
                 _launches = _launches?.sorted(by: {
                     // $0 not launched and $1 launched
@@ -150,98 +166,128 @@ final class SpaceXData: ObservableObject {
                     }
                 })
             }
-            
+
             // MARK: Crew
+
             var _crew: [Astronaut]?
             queue.addOperation { [self] in
                 _crew = loadData(url: URL(string: "https://api.spacexdata.com/v4/crew")!)
             }
-            
+
             // MARK: Launchpads
+
             var _launchpads: [Launchpad]?
             queue.addOperation { [self] in
                 _launchpads = loadData(url: URL(string: "https://api.spacexdata.com/v4/launchpads")!)
             }
-            
+
             // MARK: Landpads
+
             var _landpads: [Landpad]?
             queue.addOperation { [self] in
                 _landpads = loadData(url: URL(string: "https://api.spacexdata.com/v4/landpads")!)
             }
-            
+
             // MARK: Rockets
+
             var _rockets: [Rocket]?
             queue.addOperation { [self] in
                 _rockets = loadData(url: URL(string: "https://api.spacexdata.com/v4/rockets")!)
             }
-            
+
             // MARK: Capsules
+
             var _capsules: [Capsule]?
             queue.addOperation { [self] in
                 _capsules = loadData(url: URL(string: "https://api.spacexdata.com/v4/capsules")!)
             }
-            
+
             // MARK: Company info
+
             var _companyInfo: CompanyInfo?
             queue.addOperation { [self] in
                 _companyInfo = loadData(url: URL(string: "https://api.spacexdata.com/v4/company")!)
             }
-            
+
             // MARK: Cores
+
             var _cores: [Core]?
             queue.addOperation { [self] in
                 _cores = loadData(url: URL(string: "https://api.spacexdata.com/v4/cores")!)
             }
-            
+
             // MARK: Dragons
+
             var _dragons: [Dragon]?
             queue.addOperation { [self] in
                 _dragons = loadData(url: URL(string: "https://api.spacexdata.com/v4/dragons")!)
             }
-            
+
+            // MARK: Fairings
+
+            var _fairings: [Fairing]?
+            queue.addOperation { [self] in
+                _fairings = loadData(url: URL(string: "https://api.spacexdata.com/v4/fairings")!)
+            }
+
+            // MARK: History
+
+            var _history: [HistoryEntry]?
+            queue.addOperation { [self] in
+                _history = loadData(url: URL(string: "https://api.spacexdata.com/v4/history")!)
+            }
+
             queue.waitUntilAllOperationsAreFinished()
-            
+
             // Sync
             DispatchQueue.main.async {
                 if let _launches = _launches {
                     self.launches = _launches
                 }
-                
+
                 if let _crew = _crew {
                     self.crew = _crew
                 }
-                
+
                 if let _launchpads = _launchpads {
                     self.launchpads = _launchpads
                 }
-                
+
                 if let _landpads = _landpads {
                     self.landpads = _landpads
                 }
-                
+
                 if let _rockets = _rockets {
                     self.rockets = _rockets
                 }
-                
+
                 if let _capsules = _capsules {
                     self.capsules = _capsules
                 }
-                
+
                 if let _companyInfo = _companyInfo {
                     self.companyInfo = _companyInfo
                 }
-                
+
                 if let _cores = _cores {
                     self.cores = _cores
                 }
-                
+
                 if let _dragons = _dragons {
                     self.dragons = _dragons
+                }
+
+                if let _fairings = _fairings {
+                    self.fairings = _fairings
+                }
+
+                if let _history = _history {
+                    self.history = _history
                 }
             }
         }
     }
-    
+
     /// Creates a new instance of `SpaceXData`.
     init() {
         loadAllData()
